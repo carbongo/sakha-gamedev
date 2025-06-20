a0c0c780366d9df470ac052607bb1f2388a74299
Georgii Antipin <30941586+carbongo@users.noreply.github.com>
2025-06-20 13:16:56 +0500

    style: Improve header spacing and text styling in MainLayout component

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -10,22 +10,31 @@ export const MainLayout = ({ children, ...props }: MainLayout) => {
       className="flex flex-col min-h-screen bg-slate-100 m-0 w-full"
       {...props}
     >
-      <header className="flex flex-col gap-2 p-8 text-center">
+      <header className="flex flex-col gap-6 p-8 text-center">
         <h1 className="text-2xl font-bold text-center flex items-center gap-2 justify-center">
           Sakha GameDev<small className="text-sm text-slate-400">beta</small>
         </h1>
-        <p className="text-center text-slate-600">
+        <p className="text-center text-slate-600 block">
           A community-driven list of games made by Sakha developers.
           <br /> Support our Sakha developers by playing their games and sharing
           your feedback!
         </p>
-        <p>
-          Contribute on{" "}
+        <p className="text-center text-slate-400 block">
+          Made with ❤️ by{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://carbongo.space"
+            className="underline text-slate-500"
+          >
+            Georgii Antipin
+          </a>
+          . Contribute on{" "}
           <a
             target="_blank"
             rel="noopener noreferrer"
             href="https://github.com/carbongo/sakha-gamedev"
-            className="underline text-slate-900"
+            className="underline text-slate-500"
           >
             GitHub
           </a>
