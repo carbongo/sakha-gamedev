44e24abc5e9d35cad61478f4f4e103044c58ba82
Georgii Antipin <30941586+carbongo@users.noreply.github.com>
2025-06-20 13:04:27 +0500

    style: Refactor MainLayout component for improved structure and styling consistency

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -7,21 +7,21 @@ interface MainLayout extends React.HTMLAttributes<HTMLDivElement> {
 export const MainLayout = ({ children, ...props }: MainLayout) => {
   return (
     <div
-      className="flex flex-col h-screen bg-slate-100 p-4 w-screen gap-4"
+      className="flex flex-col h-screen bg-slate-100 px-4 w-screen gap-4"
       {...props}
     >
-      <header className="flex flex-col gap-2">
-        <h1 className="text-2xl font-bold text-center">Sakha GameDev</h1>
-        <p className="text-center text-slate-600">
-          A community-driven list of games made by Sakha developers.
-          <br /> Support our Sakha developers by playing their games and sharing
-          your feedback!
-        </p>
-      </header>
-      <main className="flex-1 overflow-y-auto">
+      <main className="flex-1 overflow-y-auto m-0 p-0">
+        <header className="flex flex-col gap-2 p-4">
+          <h1 className="text-2xl font-bold text-center">Sakha GameDev</h1>
+          <p className="text-center text-slate-600">
+            A community-driven list of games made by Sakha developers.
+            <br /> Support our Sakha developers by playing their games and
+            sharing your feedback!
+          </p>
+        </header>
         <div className="container mx-auto py-4">{children}</div>
       </main>
-      <footer className="text-center text-sm text-slate-600 flex gap-2 flex-col">
+      <footer className="text-center text-sm text-slate-600 flex gap-2 flex-col p-2">
         {/* <nav className="text-center flex gap-2 justify-center">
           <a href="/" className="text-slate-900 hover:underline mx-2">
             Our Games
